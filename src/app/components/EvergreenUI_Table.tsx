09e7b765ee8bc3dcd5360ef692e98fe70ffdd54e
CodeNerd-Utkarsh <hbase_groups_official@aol.com>
2023-07-16 10:59:40 +0530

    created two new components using 2 different libraries and imported them into page.tsx

---
+++ src/app/components/EvergreenUI_Table.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function EvergreenUI_Table({ }: Props) {
+    return (
+        <div>EvergreenUI_Table</div>
+    )
+}
+
+export default EvergreenUI_Table
---
+++ src/app/components/NextUI_Table.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function NextUI_Table({ }: Props) {
+    return (
+        <div>NextUI_Table</div>
+    )
+}
+
+export default NextUI_Table
